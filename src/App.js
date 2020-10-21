85e5afd86f4827058f04e02f238e951f45dacef6
Olga <aquapura87@gmail.com>
2020-10-22 01:25:12 +0300

    22.10.2020

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import {Menu} from "./components/Menu";
-import BrowserRouter from "react-router-dom/modules/BrowserRouter";
+import  {Route,BrowserRouter} from "react-router-dom";
+import {Pages} from "./pages/Pages";
+import {AddPage} from "./pages/AddPage";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
               <div className="row">
                   <Menu/>
                   <div className="col-9">
-
+                      <Route exact path="/" render={()=>"CMS"}/>
+                      <Route exact path="/pages/" render={()=><Pages/>}/>
+                      <Route path="/pages/addPage" render={()=><AddPage/>}/>
                   </div>
               </div>
           </BrowserRouter>
